53b6c1fb5e095e9f4d244513cec49935fb487b5b
Christophe Desguez <christophe.desguez@gmail.com>
2012-05-07 22:22:06 +0200

    Don't ignore members whose total could be zero

--- js/jquery.visualize.radar.js
+++ js/jquery.visualize.radar.js
@@ -16,11 +16,11 @@ $.visualize.plugins.radar = function () {
         container.addClass('visualize-pie-outside');
     }
 
-    var centerx = Math.round(canvas.width() / 2);
-    var centery = Math.round(canvas.height() / 2);
+    var centerX = Math.round(canvas.width() / 2);
+    var centerY = Math.round(canvas.height() / 2);
 
     var area_span = Math.PI * 2 / memberCount;
-    var radius = (centery < centerx ? centery : centerx) - o.pieMargin;
+    var radius = (centerY < centerX ? centerY : centerX) - o.pieMargin;
 
     var labels = $('<ul class="visualize-labels"></ul>').insertAfter(canvas);
 
@@ -33,61 +33,58 @@ $.visualize.plugins.radar = function () {
         ctx.beginPath();
         ctx.lineWidth = 2;
         ctx.lineJoin = 'round';
-        ctx.moveTo(centerx, centery);
+        ctx.moveTo(centerX, centerY);
         ctx.lineTo(
-            centerx + Math.cos(i * area_span) * distance,
-            centery + Math.sin(i * area_span) * distance
+            centerX + Math.cos(i * area_span) * distance,
+            centerY + Math.sin(i * area_span) * distance
         );
         ctx.strokeStyle = dataGroups[i].color;
         ctx.stroke();
         ctx.closePath()
     });
 
-    // Draw the pie pieces
+    // Draw the surrounding lines
     ctx.beginPath();
     ctx.lineWidth = 1;
     ctx.lineJoin = 'round';
     ctx.strokeStyle = o.colors[memberCount];
 
     ctx.moveTo(
-        centerx + radius * memberTotals[0] / topValue,
-        centery
+        centerX + radius * memberTotals[0] / topValue,
+        centerY
     );
 
     $.each(memberTotals, function (i, total) {
 
-        if (total > 0) {
+        var ratio = total / topValue;
+        var distance = radius * ratio;
+        var labelX = centerX + Math.cos(i * area_span) * distance;
+        var labelY = centerY + Math.sin(i * area_span) * distance;
+        ctx.lineTo(labelX, labelY);
 
-            var ratio = total / topValue;
-            var distance = radius * ratio;
-            var labelx = centerx + Math.cos(i * area_span) * distance;
-            var labely = centery + Math.sin(i * area_span) * distance;
-            ctx.lineTo(labelx, labely);
+        // draw labels
+        labelY += (labelY > centerY ? radius / 16 : -radius / 16);
 
-            // draw labels
-            labely += (labely > centery ? radius / 16 : -radius / 16);
+        var leftRight = (labelX > centerX) ? 'right' : 'left';
+        var topBottom = (labelY > centerY) ? 'bottom' : 'top';
 
-            var leftRight = (labelx > centerx) ? 'right' : 'left';
-            var topBottom = (labely > centery) ? 'bottom' : 'top';
+        var labeltext = $("<span>")
+            .addClass("visualize-label")
+            .html(total)
+            .css(leftRight, 0)
+            .css(topBottom, 0)
+            .css('font-size', radius / 8)
+            .css('color', dataGroups[i].color);
 
-            var labeltext = $("<span>")
-                .addClass("visualize-label")
-                .html(total)
-                .css(leftRight, 0)
-                .css(topBottom, 0)
-                .css('font-size', radius / 8)
-                .css('color', dataGroups[i].color);
+        $("<li>")
+            .addClass("visualize-label-pos")
+            .css({left:labelX, top:labelY})
+            .append(labeltext)
+            .appendTo(labels);
 
-            $("<li>")
-                .addClass("visualize-label-pos")
-                .css({left:labelx, top:labely})
-                .append(labeltext)
-                .appendTo(labels);
-
-            labeltext
-                .css('margin-' + leftRight, -labeltext.width() / 2)
-                .css('margin-' + topBottom, -labeltext.outerHeight() / 2)
-        }
+        labeltext
+            .css('margin-' + leftRight, -labeltext.width() / 2)
+            .css('margin-' + topBottom, -labeltext.outerHeight() / 2)
 
     });
 
