798cd133232caa7e2da4f93d8ac3d4d68f9c0276
Christophe Desguez <christophe.desguez@gmail.com>
2012-05-15 21:10:39 +0200

    Lot of debug.. The EnhancedCanvas methods to define the viewport setWindow() now works as intended, as can be seen with drawAxis()

--- js/jquery.visualize.2.0.0.js
+++ js/jquery.visualize.2.0.0.js
@@ -4,215 +4,269 @@
  * Date: 14/05/12
  * Time: 12:38
  */
-(function($) {
+(function ($) {
 
 // UTILITIES
-Array.max = function(arr) {
-    return Math.max.apply(Array, arr);
-};
-Array.min = function(arr) {
-    return Math.min.apply(Array, arr);
-};
-Array.sum = function(arr) {
-    var len = (arr && arr.length ? arr.length : 0), sum = 0, val;
-    for (var i = 0; i < len; i++) {
-        val = parseFloat(arr[i]);
-        sum += ((!arr[i] || isNaN(arr[i])) ? 0 : val);
+    Array.max = function (arr) {
+        return Math.max.apply(Array, arr);
+    };
+    Array.min = function (arr) {
+        return Math.min.apply(Array, arr);
+    };
+    Array.sum = function (arr) {
+        var len = (arr && arr.length ? arr.length : 0), sum = 0, val;
+        for (var i = 0; i < len; i++) {
+            val = parseFloat(arr[i]);
+            sum += ((!arr[i] || isNaN(arr[i])) ? 0 : val);
+        }
+        return sum;
+    };
+    Array.avg = function (arr) {
+        var len = (arr && arr.length ? arr.length : 0);
+        return (len ? Array.sum(arr) / len : 0);
+    };
+
+    // Build a generic representation of the data scraped from the table
+    function scrapeTable(table) {
     }
-    return sum;
-};
-Array.avg = function(arr) {
-    var len = (arr && arr.length ? arr.length : 0);
-    return (len ? Array.sum(arr)/len : 0);
-};
 
+    /**
+     * Multi dimensional structure to store series of data
+     * and easily retrieve them
+     * @param stats
+     */
+    function Stats(stats) {
 
-/**
- * Multi dimensional structure to store series of data
- * and easily retrieve them
- * @param stats
- */
-function Stats(stats) {
+        var defaultSelf = {
+            title:"Empty",
+            description:"not provided",
+            series:{
+                lines:[],
+                columns:[]
+            }
+        };
 
-    var defaultSelf = {
-        title: "Empty",
-        description: "not provided",
-        series: {
-            lines: [],
-            columns: []
+        if (typeof stats == "string") {
+            this.table = $(stats);
+            $.extend(this, scrapeTable(this.table));
+
+        } else {
+            $.extend(this, defaultSelf, stats);
         }
     }
 
-    if (typeof stats == "string") {
-        this.table = $(stats);
-        $.extend(this, scrapeTable(this.table));
-    } else {
-        $.extend(this, defaultSelf, stats);
-    }
-}
-
-Stats.prototype = {
-    max: function() {
-        var dataSet, len = arguments.length;
-        if (len == 1) {
-            return Array.max(this.series[arguments[0]]);
+    Stats.prototype = {
+        max:function () {
+            var dataSet, len = arguments.length;
+            if (len == 1) {
+                return Array.max(this.series[arguments[0]]);
+            }
         }
+    };
+
+
+    function EnhancedCanvas($canvas, container) {
+        this.canvas = $canvas[0];
+        this.width  = $canvas.width();
+        this.height = $canvas.height();
+        this.canvasContainer = container;
+        this.canvasContext = this.canvas.getContext('2d');
     }
-};
+
+    EnhancedCanvas.prototype = {
+        /**
+         * Set the coordinate system
+         * @param coordRange {x: range array, y: range array}
+         */
+        setWindow: function(coordRange) {
+            this.coordRange = coordRange;
+
+            var xRange = (coordRange.x[1] - coordRange.x[0]);
+            var xScale = this.width / xRange;
+
+            var yRange = (coordRange.y[0] - coordRange.y[1]); // reverse orientation for the y axis
+            var yScale = this.height / yRange;
+
+            this.canvasContext.scale(xScale, yScale); // this makes the viewport scale to the desired range
+            this.canvasContext.translate(-1*coordRange.x[0], -1*coordRange.y[1]); // this map the center where it should be
+
+        },
+
+        drawAxis: function() {
+            var ctx = this.canvasContext;
+
+            // Draw the X axis
+            ctx.beginPath();
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 1;
+            ctx.moveTo(this.coordRange.x[0], 0);
+            ctx.lineTo(this.coordRange.x[1], 0);
+            ctx.stroke();
+
+            // Draw the Y axis
+            ctx.beginPath();
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 1;
+            ctx.moveTo(0, this.coordRange.y[0]);
+            ctx.lineTo(0, this.coordRange.y[1]);
+            ctx.stroke();
+        }
+    };
+
+    var defaults = {
+        type:'bar', //also available: area, pie, line
+        width:$(this).width(), //height of canvas - defaults to table height
+        height:$(this).height(), //height of canvas - defaults to table height
+
+        appendTitle:true, //table caption text is added to chart
+        title:null, //grabs from table caption if null
+        appendKey:true, //color key is added to chart
+
+        rowFilter:' ',
+        colFilter:' ',
+
+        colors:['#be1e2d', '#666699', '#92d5ea', '#ee8310', '#8d10ee', '#5a3b16', '#26a4ed', '#f45a90', '#e9e744'],
+        textColors:[], //corresponds with colors array. null/undefined items will fall back to CSS
+
+        parseDirection:'x', //which direction to parse the table data
+        lineWeight:4, //for line and area - stroke weight
+        yLabelInterval:30, //distance between y labels
+
+        pieMargin:20, //pie charts only - spacing around pie
+        pieLabelsAsPercent:true,
+        pieLabelPos:'inside',
+
+        barGroupMargin:10,
+        barMargin:1  //space around bars in bar chart (added to both sides of bar)
+    };
+
+    $.visualize = function (rawData, opts) {
+
+        var options = $.extend({}, defaults, opts);
+        var stats = new Stats(rawData);
+
+        //create canvas wrapper div, set inline w&h, append
+        var canvasContainer;
+        if (options.container) {
+            canvasContainer = $(options.container);
+            if (canvasContainer.length == 0) throw "[jquery.visualize plugin error] The target container '" + options.container + "' didn't return any matching element.";
+
+        } else {
+            canvasContainer = $("<div>")
+                .addClass("visualize")
+                .attr("role", "img")
+                .attr("aria-label", "Graph for " + stats.title)
+                .height(options.height).width(options.width)
+            ;
+        }
+
+        //create new canvas, set w&h attrs (not inline styles)
+        var canvas = $("<canvas>")
+            .height(options.height)
+            .width(options.width)
+            .appendTo(canvasContainer)
+        ;
+
+        //title/key container
+        if (options.appendTitle || options.appendKey) {
+
+            var infoContainer = $('<div class="visualize-info"></div>')
+                .appendTo(canvasContainer);
+
+            //append title
+            if (options.appendTitle) {
+                $("<div>")
+                    .addClass("visualize-title")
+                    .html(stats.title)
+                    .appendTo(infoContainer);
+            }
+
+            //append key
+            if (options.appendKey) {
+                var legend = $("<ul>").addClass("visualize-key");
+                $.each(stats.series.lines, function (i, line) {
+                    $("<li>")
+                        .append(
+                        $("<span>")
+                            .addClass("visualize-key-color")
+                            .css({background:options.colors[i]})
+                    ).append(
+                        $("<span>")
+                            .addClass("visualize-key-label")
+                            .html(line.name)
+                    ).appendTo(legend);
+                });
+                legend.appendTo(infoContainer);
+            }
+        }
+
+        // Append new canvas to page
+        if (!options.container) {
+            if (stats.table) {
+                canvasContainer.insertAfter(stats.table);
+            } else {
+                canvasContainer.appendTo("body");
+            }
+        }
+
+        // Something strange (maybe a IE hack)
+        // @TODO TEST OR SUPPRESS THIS
+        if (typeof(G_vmlCanvasManager) != 'undefined') {
+            G_vmlCanvasManager.init();
+            G_vmlCanvasManager.initElement(canvas[0]);
+        }
+
+        // Create chart
+        var type = options.type,
+            context = {
+                data:stats,
+                options:options,
+                target: new EnhancedCanvas(canvas, canvasContainer)
+            };
+
+        if ($.visualize.plugins[type]) {
+            $.visualize.plugins[type].apply(context); // call our external plugin with the passed context
+
+        } else {
+            // try to dynamically load a new type of chart from external plugin
+            console.log("Trying to load jquery.visualize." + type + ".js");
+
+            $.getScript("./js/jquery.visualize." + type + ".js",
+                function loaded() {
+                    $.visualize.plugins[type].apply(context);
+                }).fail(function (jqxhr, settings, exception) {
+                    context.target.canvasContainer.remove();
+                    throw "[jquery.visualize plugin error] Failed to load jquery.visualize plugin " + type + " : " + exception;
+                });
+        }
+
+    };
+
+    $.visualize.plugins = {}; // additional chart scripts will load inside this namespace
 
 
-var defaults = {
-		type: 'bar', //also available: area, pie, line
-		width: $(this).width(), //height of canvas - defaults to table height
-		height: $(this).height(), //height of canvas - defaults to table height
+    /**
+     * Call the visualize plugin on tables to scrap their data
+     * and automatically display a resulting charts
+     * @param options
+     * @param container the target element where to display the chart. eq to pass it as options.container
+     */
+    $.fn.visualize = function (options, container) {
 
-		appendTitle: true, //table caption text is added to chart
-		title: null, //grabs from table caption if null
-		appendKey: true, //color key is added to chart
+        if (container) {
+            options.container = $(container);
+        }
 
-		rowFilter: ' ',
-		colFilter: ' ',
+        return $(this).each(function (i, table) {
 
-		colors: ['#be1e2d','#666699','#92d5ea','#ee8310','#8d10ee','#5a3b16','#26a4ed','#f45a90','#e9e744'],
-		textColors: [], //corresponds with colors array. null/undefined items will fall back to CSS
+            if (table.tagName != "TABLE") throw "[jquery.visualize plugin error] We don't know how to scrap anything else than HTML <table>(s)";
 
-		parseDirection: 'x', //which direction to parse the table data
-		lineWeight: 4, //for line and area - stroke weight
-		yLabelInterval: 30, //distance between y labels
+            //configuration
+            $.visualize(stats, options);
 
-		pieMargin: 20, //pie charts only - spacing around pie
-		pieLabelsAsPercent: true,
-		pieLabelPos: 'inside',
+        });
 
-		barGroupMargin: 10,
-		barMargin: 1  //space around bars in bar chart (added to both sides of bar)
-};
-
-$.visualize = function(rawData, opts) {
-
-	var options = $.extend({}, defaults, opts);
-	var stats = new Stats(rawData);
-
-	//create new canvas, set w&h attrs (not inline styles)
-	var canvas = $("<canvas>")
-		.height(options.height)
-		.width(options.width);
-
-	//create canvas wrapper div, set inline w&h, append
-	var canvasContain = (options.container || 
-		$("<div>")
-			.addClass("visualize")
-			.attr("role", "img")
-			.attr("aria-label", "Graph for " + stats.title)
-			.height(options.height).width(options.width))
-		.append(canvas);
-
-	//title/key container
-	if (options.appendTitle || options.appendKey) {
-
-		var infoContainer = $('<div class="visualize-info"></div>')
-			.appendTo(canvasContain);
-
-		//append title
-		if (options.appendTitle) {
-			$("<div>")
-				.addClass("visualize-title")
-				.html(stats.title)
-				.appendTo(infoContainer);
-		}
-
-		//append key
-		if (options.appendKey) {
-			var legend = $("<ul>").addClass("visualize-key");
-			stats.series.lines(function(i, line) {
-				$("<li>")
-					.append(
-						$("<span>")
-							.addClass("visualize-key-color")
-							.css({background: options.colors[i])
-					).append(
-						$("<span>")
-							.addClass("visualize-key-label")
-							.html(line.name)
-					).appendTo(legend);
-			});
-			legend.appendTo(infoContainer);
-		}
-	}
-
-	// Append new canvas to page	
-	if (!container) {
-		if (stats.table) {
-			canvasContainer.insertAfter(stats.table);
-		} else {
-			canvasContainer.appendTo("body");
-		}
-	}
-	
-	// Something strange (maybe a IE hack)
-	// @TODO TEST OR SUPPRESS THIS
-	if (typeof(G_vmlCanvasManager) != 'undefined') { 
-		G_vmlCanvasManager.init(); 
-		G_vmlCanvasManager.initElement(canvas[0]); 
-	}	
-
-	// Create chart
-	var type = options.type, 
-			context = {
-				data: stats;
-				options: options,
-				target: {
-					canvasContainer: canvasContain,
-					canvasContext: canvas[0].getContext('2d'),
-					canvas: canvas					
-				}
-			};
-
-	if ($.visualize.plugins[type]) {
-			$.visualize.plugins[type].apply(context); // call our external plugin with the passed context
-
-	} else {
-			// try to dynamically load a new type of chart from external plugin
-			console.log("Trying to load jquery.vizualize." + type + ".js");
-
-			$.getScript("./js/jquery.visualize." + type + ".js", function loaded() {
-					$.visualize.plugins[type].apply(context);
-			}).fail(function(jqxhr, settings, exception) {
-					context.target.canvasContainer.remove();
-					throw "Failed to load jquery.vizualize plugin " + type + " : " + exception;
-			});
-	}
-
-}
-
-$.visualize.plugins: {} // additional chart scripts will load inside this namespace
-
-
-/**
- * Call the visualize plugin on tables to scrap their data
- * and automatically display a resulting charts
- * @param options
- * @param container
- */
-$.fn.visualize = function(opts, container) {
-
-		// Build a generic representation of the data scraped from the table
-		function scrapeTable(table) {
-		}
-
-		if (container) {
-			opts.container = $(container);
-		}
-
-    return $(this).each(function() {
-
-        //configuration
-				var stats = scrapeTable($(this));
-				$.visualize(stats, opts);
-
-    });
-
-};
+    };
 
 
 })(jQuery);
--- js/jquery.visualize.radar.js
+++ js/jquery.visualize.radar.js
@@ -7,82 +7,79 @@
  */
 $.visualize.plugins.radar = function () {
 
-    var options = this.options,
-        container = this.target.canvasContainer,
-        ctx = this.target.canvasContext,
-        canvas = this.target.canvas,
-        data = this.data;
+	var options = this.options,
+		container = this.target.canvasContainer,
+		ctx = this.target.canvasContext,
+		canvas = this.target.canvas,
+		data = this.data;
 
 
-    container.addClass('visualize-pie');
+	container.addClass('visualize-pie');
 
-    if (options.pieLabelPos == 'outside') {
-        container.addClass('visualize-pie-outside');
-    }
+	if (options.pieLabelPos == 'outside') {
+		container.addClass('visualize-pie-outside');
+	}
 
-		var max = data.series.lines.max();
-		var radius = max + options.pieMargin;
+	var max = data.series.lines.max,
+        count = data.series.lines.length;
+	var radius = max + options.pieMargin;
 
-		// Tell the drawing area (canvas) to be the center of a cicrcle with the given radius
-		var tx = this.target.setWindow(-radius, radius, -radius, radius);  
-		ctx.setTransformation(tx);
+	// Tell the drawing area (canvas) to be the center of a circle with the given radius
+	this.target.setWindow({x: [-radius, radius], y: [-radius, radius]});
 
-    var area_span = Math.PI * 2 / this.data.series.lines.count();
+	var area_span = Math.PI * 2 / count;
 
 
-    // Draw the branches of our star shape
-    $.each(data.series.lines, function (i, line) {
+	// Draw the branches of our star shape
+	$.each(data.series.lines, function (i, line) {
 
-        var l = line.sum();
+		var l = Array.sum(line.values);
 
-        ctx.beginPath();
-        ctx.lineWidth = options.lineWeight;
-        ctx.lineJoin = 'round';
-        ctx.moveTo(0, 0);
-        ctx.lineTo(
-            l * Math.cos(i * area_span),
-            l * Math.sin(i * area_span)
-        );
-        ctx.strokeStyle = options.colors[i];
-        ctx.stroke();
-        ctx.closePath();
-    });
+		ctx.beginPath();
+		ctx.lineWidth = options.lineWeight;
+		ctx.lineJoin = 'round';
+		ctx.moveTo(0, 0);
+		ctx.lineTo(
+			l * Math.cos(i * area_span),
+			l * Math.sin(i * area_span)
+		);
+		ctx.strokeStyle = options.colors[i];
+		ctx.stroke();
+		ctx.closePath();
+	});
 
-    // Draw the surrounding form
-    var labels = $('<ul class="visualize-labels"></ul>').insertAfter(canvas);
+	// Draw the surrounding form
+	var labels = $('<ul class="visualize-labels"></ul>').insertAfter(canvas);
 
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.lineJoin = 'round';
-    ctx.strokeStyle = options.colors[memberCount];
+	ctx.beginPath();
+	ctx.lineWidth = 1;
+	ctx.lineJoin = 'round';
+	ctx.strokeStyle = options.colors[count];
 
-    ctx.moveTo(
-        centerX + radius * memberTotals[0] / topValue / 2,
-        centerY
-    );
+	ctx.moveTo(line.values[0], 0);
 
-    $.each(data.series.lines, function (i, line) {
+	$.each(data.series.lines, function (i, line) {
 
-        var ratio = line.sum() / max;
-        var l = radius * ratio;
+		var val = Array.sum(line.values), ratio = val / max;
+		var l = radius * ratio;
 
-        var labelX = l * Math.cos(i * area_span);
-        var labelY = l * Math.sin(i * area_span);
-        ctx.lineTo(labelX, labelY);
+		var labelX = l * Math.cos(i * area_span);
+		var labelY = l * Math.sin(i * area_span);
+		ctx.lineTo(labelX, labelY);
 
-        // draw labels
-        labelY += (labelY > centerY ? radius / 16 : -radius / 16);
+		// draw labels
+		labelY += (labelY > centerY ? radius / 16 : -radius / 16);
 
-        var leftRight = (labelX > centerX) ? 'right' : 'left';
-        var topBottom = (labelY > centerY) ? 'bottom' : 'top';
+		var leftRight = (labelX > centerX) ? 'right' : 'left';
+		var topBottom = (labelY > centerY) ? 'bottom' : 'top';
 
-        var labeltext = $("<span>")
-            .addClass("visualize-label")
-            .html(line.sum())
-            .css(leftRight, 0)
-            .css(topBottom, 0)
-            .css('font-size', radius / 8)
-            .css('color', options.colors[i});
+		var labeltext = $("<span>")
+			.addClass("visualize-label")
+			.html(val)
+			.css(leftRight, 0)
+			.css(topBottom, 0)
+			.css('font-size', radius / 8)
+			.css('color', options.colors[i]);
 
         $("<li>")
             .addClass("visualize-label-pos")
@@ -93,9 +90,10 @@ $.visualize.plugins.radar = function () {
         labeltext
             .css('margin-' + leftRight, -labeltext.width() / 2)
             .css('margin-' + topBottom, -labeltext.outerHeight() / 2);
-
-    });
+	});
 
     ctx.closePath();
     ctx.stroke();
+
 };
+
