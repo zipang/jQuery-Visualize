46b07c4331fcd307e47c819950738c51c64a0338
Christophe Desguez <christophe.desguez@gmail.com>
2012-05-30 02:18:18 +0200

    Completed DataSet creation from a static structure

---
+++ js/DataSet.js
@@ -0,0 +1,102 @@
+/**
+ * DataSet: Gather serie of values into coherent views (or axis of analysis)
+	 Multiple series are grouped into categories that usually data views
+	 along an axis.
+
+	Example :
+
+	var sales = new DataSet("Representants sales per business categories");
+
+	var johnSales = new Serie("John", 1000, 500, 890).keys("Food", "Electronics", "Cars");
+	var bettiesSales = new Serie("Bettie", 635, 890, 2130).keys("Food", "Electronics", "Cars");
+	var terekSales = new Serie("Terek", 890, 1210, 1754).keys("Food", "Electronics", "Cars");
+
+	sales.add("lines", [johnSales, bettiesSales, terekSales]);
+
+	// Add the same data considered along another axis
+	var foodSales = new Serie("Food", 1000, 635, 890).keys("John", "Bettie", "Terek");
+	var electronicSales = new Serie("Electronic", 500, 890, 1210).keys("John", "Bettie", "Terek");
+	var carSales = new Serie("Cars", 890, 2130, 1754).keys("John", "Bettie", "Terek");
+
+	sales.add("columns", [foodSales, electronicSales, carSales]);
+
+	// We can now access these data in multiple ways :
+
+	sales.columns.Cars.John >> 890
+	sales.lines.John.Cars >> 890
+
+	sales.columns.Cars.max() >> 2130
+	sales.lines.John.max() >> 1000
+
+*/
+
+// import dependancies in
+var Serie = this.Serie ? this.Serie : require("./Serie").Serie;
+
+// Define the DataSet constructor in the given namespace.
+(function define(namespace) {
+
+	/**
+	* Multi	dimensional structure to store series of data
+	* and easily retrieve them
+	* @param stats
+	*/
+	function DataSet(stats) {
+
+		var defaultSelf	= {
+			title:"Empty",
+			description:"not provided",
+			series:{}
+		};
+
+		if (arguments.length == 0 || !stats) {
+			return defaultSelf;
+
+		} else if (arguments.length == 1 && stats.constructor == Object) { // Object literal
+			this.title = stats.title || defaultSelf.title;
+			this.description = stats.description || defaultSelf.description;
+
+			this.series = {};
+
+			for (var catgName in stats.series) {
+				var catg = this.series[catgName] = new Serie(catgName); // 'lines' or 'colums' for example
+				var data = stats.series[catgName];
+
+				var serieKeys = data.keys, catgKeys = [];
+				delete data.keys;
+
+				for (var name in data) {
+					var s = new Serie(name, data[name]).keys(serieKeys);
+					catg.push(s);
+					catgKeys.push(name);
+				}
+				catg.keys(catgKeys);
+
+			}
+
+		} else if (typeof stats == "string") {
+			this.title = stats;
+			this.series = {};
+
+		}
+
+	} // DataSet constructor
+
+	DataSet.prototype = {
+
+		addSerie: function(catgName, serie) {
+			if (arguments.length != 2 || !(serie instanceof Serie)) {
+				throw "Bad arguments : expected a categorie name and a serie"
+			}
+			if (!this[catgName]) this.addCategorie(catgName);
+			this[catgName].push(serie);
+		}
+	}; // DataSet prototype
+
+	// ------------------------------------------------------ //
+
+
+	// Return our Serie constructor in the given namespace.
+	namespace.DataSet = DataSet;
+
+})((module) ? module.exports : window);
---
+++ js/tests/dataset-tests.js
@@ -0,0 +1,79 @@
+
+var Serie   = require("../Serie").Serie,
+		DataSet = require("../DataSet").DataSet,
+		vows		= require("vows"),
+	should		= require("should");
+
+
+function rawData(title) {
+	return {
+		title: title,
+		description: "Sales by departments and salesman",
+		series: {
+			lines: {
+				keys: ["Cars", "Food", "Electronic"],
+				"John": [1500, 340, 890],
+				"Bettie": [2000, 450, 210],
+				"Paul": [0, 890, 1500],
+			},
+			columns: {
+				keys: ["John", "Bettie", "Paul"],
+				"Cars": [1500, 2000, 0],
+				"Food": [340, 450, 890],
+				"Electronic": [890, 210, 1500]
+			},
+		}
+	};
+}
+
+function dataSet(title) {
+	return new DataSet(rawData(title));
+}
+
+
+vows.describe("A multidimensional set of data").addBatch({
+
+	"Data set initialization": {
+
+		topic: function() {
+			return new DataSet("Sales");
+		},
+
+		"Check name and initial length": function (data) {
+			data.should.have.keys(["title", "series"]);
+			data.title.should.equal("Sales");
+		}
+	},
+
+	"Create a DataSet from raw structure": {
+
+		topic: function() {
+			return dataSet("Sales");
+		},
+
+		"Check name and initial length": function (data) {
+			data.should.have.keys(["title", "description", "series"]);
+			data.title.should.equal("Sales");
+		},
+
+		"Check series existence": function (data) {
+			data.series.should.have.keys(["lines", "columns"]);
+		},
+
+		"Check lines content": {
+			topic: function (data) {return data.series.lines;},
+
+			"lines is a Serie": function (lines) {
+				should.exist(lines);
+				lines.constructor.should.eql(Serie);
+				should.ok(lines.length == 3);
+			},
+
+			"lines has aliased values": function (lines) {
+				should.exist(lines.John);
+			}
+
+		}
+	}
+
+}).export(module); // End of batch
