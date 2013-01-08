01374bdf9d7e152102bdc687aa7b20a182d12fdf
Christophe Desguez <christophe.desguez@gmail.com>
2013-01-08 23:22:11 +0100

    [FEATURE] New 'column' option allow to take data from only one column. [FIXED] Display the correct slice value when option pieLabelsAsPercent=false. [REFACTORING] Rapatried the defaults.

--- js/plugins/jquery.visualize.pie.js
+++ js/plugins/jquery.visualize.pie.js
@@ -4,37 +4,69 @@
  * Data are represented by colored slices of a pie.
  */
 (function define() {
-	var pie = $.visualize.plugins.pie = function () {
+
+	var defaults = {
+		pieMargin:20, //pie charts only - spacing around pie
+		pieLabelsAsPercent:false,
+		pieLabelPos:'inside',		
+	};
+	
+	$.visualize.plugins.pie = function () {
 
 		var o = this.options,
 			ctx = this.target.canvasContext,
 			$canvas = this.target.canvas,
 			w = $canvas.width(), h = $canvas.height(),
-			tabledata = this.data,
+			tabledata = this.data;
 
-			data = (o.parseDirection == 'x') ? tabledata.lines : tabledata.columns,
-			seriesTotal = $.map(data, Array.sum),
-			grandTotal = Array.sum(seriesTotal);
+		// Let's gather the pie data
+		var slices = [], keys = [], total;
 
-		// legend keys
-		this._keys = (o.parseDirection == 'x') ? tabledata.lineHeaders : tabledata.columnHeaders;
+		if (o.column) { // extract data from a single column
+			var input = tabledata.get("columns", o.column), 
+				type  = o.columnType || isNaN(input[0]) ? "text" : "value",
+				stats = {};
+			$.each(input, function(i, dat) {
+				if (type == "value") { // take this value
+					stats[tabledata.lineHeaders[i]] = dat;
+				} else { // count the number of items with the same value
+					if (!stats[dat]) stats[dat] = 1; else stats[dat] += 1;
+				}
+			});
+			for (var key in stats) {
+				keys.push(key);
+				slices.push(stats[key]);
+			}
+		} else { // extract data from the whole lines/columns (sum the values)
+			if (o.parseDirection == 'x') { // series are lines
+				slices = $.map(tabledata.lines, Array.sum);
+				keys = tabledata.lineHeaders;
+			} else {
+				slices = $.map(tabledata.columns, Array.sum);
+				keys = tabledata.columnHeaders;
+			}
+		}
+			
+		total = Array.sum(slices);
+		this._keys = keys;
 
+		// Let's draw the pie
 		if (o.pieLabelPos == 'outside') {
 			this.target.container.addClass('visualize-pie-outside');
 		}
+
+		var labels = $('<ul class="visualize-labels"></ul>')
+			.insertAfter($canvas);
 
 		var centerX = Math.round(w / 2),
 			centerY = Math.round(h / 2),
 			radius = centerY - o.pieMargin,
 			counter = 0.0;
 
-		var labels = $('<ul class="visualize-labels"></ul>')
-			.insertAfter($canvas);
-
-		$.each(seriesTotal, function (i, total) {
+		$.each(slices, function (i, val) {
 
 			// Draw the pie pieces
-			var slice = (total <= 0 || isNaN(total)) ? 0 : total / grandTotal;
+			var slice = (val <= 0 || isNaN(val)) ? 0 : val / total;
 			if (slice > 0) {
 				ctx.beginPath();
 				ctx.moveTo(centerX, centerY);
@@ -58,7 +90,7 @@
 			var percentage = parseFloat((slice * 100).toFixed(2));
 
 			if (percentage) {
-				var labelval = (o.pieLabelsAsPercent) ? percentage + '%' : total;
+				var labelval = (o.pieLabelsAsPercent) ? percentage + '%' : val;
 				var $label = $('<span class="visualize-label">' + labelval + '</span>')
 					.css({leftRight: 0, topBottom: 0});
 				var label = $('<li class="visualize-label-pos"></li>')
@@ -74,7 +106,9 @@
 				}
 			}
 			counter += slice;
-		});
+		}); // each slices
 
-	}
+	}; // pie
+
+	$.visualize.plugins.pie.defaults = defaults;
 })();
